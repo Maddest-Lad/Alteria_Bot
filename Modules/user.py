43a32666915aed829f099ce017de0e897b2fd45a
Sam Harris <sam.career@outlook.com>
2023-05-05 12:52:02 -0700

    added more data fields to account for time zones

--- Modules/user.py
+++ Modules/user.py
@@ -7,12 +7,18 @@ class User:
     
     username: str
     id: str
-    location: str # For Weather Reports
     
-    def __init__(self, id, username, location=None):
+    # For Daily Weather Reports
+    location: str 
+    time_zone: str 
+    report_time: int # By Hour 1-24
+    
+    def __init__(self, id, username, location=None, time_zone=None, report_time=None):
         self.id = id        
         self.username = username
         self.location = location
+        self.time_zone = time_zone
+        self.report_time = report_time
         
         # Create File For New Users
         if not Path.joinpath(DATA_DIR, id).exists():
@@ -30,13 +36,15 @@ class User:
         with open(f"{Path.joinpath(DATA_DIR, self.id)}.json", 'w', encoding='utf-8') as json_file:
             json.dump(self.__dict__, json_file, skipkeys=True, indent=4)
             
-    # Set The Location (Opt into Weather Notifications), Passing None will Opt Out 
+    ### Weather Scheduler Information - Passing None will Opt Out 
     def set_location(self, location):
         self.location = location
         self.update_json()
-
-# Testing
-if __name__ == "__main__":
-    b = User.from_json("Data/165971552771244033.json")
-    b.set_location(None)
-    print(b.location)
+         
+    def set_time_zone(self, time_zone):
+        self.time_zone = time_zone
+        self.update_json()
+    
+    def set_report_time(self, report_time):
+        self.report_time = report_time
+        self.update_json()    
